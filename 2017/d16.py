7318e38fd0d6f32c52028847e2cd7dbd6d450566
Max <maaax.fischer@gmail.com>
2017-12-16 10:56:51 +0100

    Small refactoring

--- 2017/d16.py
+++ 2017/d16.py
@@ -4,12 +4,12 @@ from utils import utils
 from collections import deque
 
 
-def swap(letters: list, a: int, b: int) -> str:
+def swap(letters: list, a: int, b: int) -> list:
     letters[b], letters[a] = letters[a], letters[b]
     return letters
 
 
-def part_one(day: int, letters: list) -> None:
+def exec_moves(day: int, letters: list) -> None:
     # letters = ['a', 'b', 'c', 'd', 'e']
     moves = utils.get_input(day)
     # moves = 's1,x3/4,pe/b'
@@ -18,7 +18,7 @@ def part_one(day: int, letters: list) -> None:
     for i in range(10 ** 9):
         letters_str = ''.join(letters)
         if letters_str in done:
-            print(done[10 ** 9 % i])
+            print('Part 2: {0}'.format(done[10 ** 9 % i]))
             return
 
         done.append(letters_str)
@@ -33,16 +33,13 @@ def part_one(day: int, letters: list) -> None:
                 letters = swap(letters, int(idcs[0]), int(idcs[1]))
             elif move and move[0] == 'p':
                 letters = swap(letters, letters.index(move[1]), letters.index(move[-1]))
-
-    letters = ''.join(letters)
-    print(letters)
-
-
+        if i == 0:
+            print('Part 1: {0}'.format(''.join(letters)))
 
 
 def main():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
-    part_one(16, letters)
+    exec_moves(16, letters)
 
 
 if __name__ == '__main__':
