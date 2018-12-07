21374c59c11b30f6845171728116644315580bcd
Max Fischer <max.fischer@diva-e.com>
2018-12-07 15:14:30 +0100

    Add part 2 of day 7

--- 2018/d07.py
+++ 2018/d07.py
@@ -2,13 +2,13 @@
 
 from utils import utils
 import networkx as nx
-import matplotlib.pyplot as plt
+
 
 def create_graph(data):
     G = nx.DiGraph()
     for line in data:
         if line:
-            line_parts = line.split(' ')
+            line_parts = line.split()
             G.add_edge(line_parts[1], line_parts[7])
     starting_nodes = list((node for node in G if G.in_degree(node) == 0))
     return G, starting_nodes
@@ -24,15 +24,6 @@ def find_order(G, starting_nodes):
         to_visit = sorted(list(set(to_visit)))
         i = skip
         node = to_visit[i]
-        predecessors = set(G.predecessors(node))
-
-        # print('node', node)
-        # print('Pred.', predecessors)
-        # print('vis', visited)
-        # print(order)
-        # print(to_visit)
-        # print(i)
-        # print('#' * 10)
 
         if node in visited:
             del to_visit[i]
@@ -48,24 +39,58 @@ def find_order(G, starting_nodes):
 
         else:
             skip += 1
-            print('skip',skip)
 
-    print(order)
-    print(len(G.nodes))
     return order
+
+
+def time_task(c):
+    return 60 + ord(c) - ord("A") + 1
+
+
+def part_two(G):
+    # How much time is left for specific worker to do
+    task_times = []
+    # Tasks (nodes) currently worked on
+    tasks = []
+    # Complete time to finish: return
+    overall_time = 0
+
+    # Iterate while tasks have to be done
+    while task_times or G:
+        # Available tasks are not done yet (removed) and all pre-tasks are
+        # done
+        available_tasks = [t for t in G if t not in tasks and G.in_degree(t) == 0]
+
+        # Set task for a free worker
+        if available_tasks and len(task_times) < 5:
+            task = min(available_tasks)  # min: alphabetically first
+            task_times.append(time_task(task))
+            tasks.append(task)
+
+        # Calculate next task(s) to be done, remove and free worker(s)
+        # Add time to overall time
+        else:
+            min_time = min(task_times)
+            completed = [
+                tasks[i] for i, time in enumerate(task_times) if time == min_time
+            ]
+            task_times = [v - min_time for v in task_times if v > min_time]
+            tasks = [t for t in tasks if t not in completed]
+            overall_time += min_time
+            G.remove_nodes_from(completed)
+    return overall_time
 
 
 def main():
     data = utils.get_input(7).split("\n")
     G, starting_node = create_graph(data)
-    # nx.draw(G)
-    # plt.show()
+    # Very short and via networkX algorithm only:
+    # ''.join(nx.lexicographical_topological_sort(G)))
     order = find_order(G, starting_node)
-    order_str = ''.join(order)
-    print(order_str)
+    order_str = "".join(order)
+    print(f"Part 1: {order_str}")
 
-    # print(f"Part 1: {}")
-    # print(f"Part 2: {}")
+    print(f"Part 2: {part_two(G)}")
 
 
 if __name__ == "__main__":
