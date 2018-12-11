3d2cad79d60aebdec8fb62ce9aef017173d17cb6
Max <maaax.fischer@gmail.com>
2018-12-11 20:30:10 +0100

    Cleaned up day 11 using numpy array, speedup > 9000

--- 2018/d11.py
+++ 2018/d11.py
@@ -1,13 +1,57 @@
 #!/usr/bin/env python
 
-from collections import defaultdict
-from collections import deque
+from collections import defaultdict, deque
 import operator
 import re
 
+import numpy as np
 
 GRID_SIZE = 300
-MAX_SIZE = 40
+MAX_SIZE = 50
+ARBITRARY_LOW = -10000
+
+
+class Grid:
+    def __init__(self, serial_number):
+        self.power_levels = np.zeros((GRID_SIZE, GRID_SIZE), dtype=int)
+
+        for x in range(GRID_SIZE):
+            for y in range(GRID_SIZE):
+                rack_id = (x + 1) + 10
+                power_lvl = (y + 1) * rack_id
+                power_lvl += serial_number
+                power_lvl *= rack_id
+                power_lvl = get_hundreds_digit(power_lvl)
+                power_lvl -= 5
+                self.power_levels[(x, y)] = power_lvl
+
+    def find_max(self, size):
+        best_cell = (0, 0)
+        max_power_lvl = ARBITRARY_LOW
+
+        for x in range(GRID_SIZE - size + 1):
+            for y in range(GRID_SIZE - size + 1):
+                current_power_lvl = ARBITRARY_LOW
+
+                current_power_lvl = np.sum(
+                    self.power_levels[x : x + size, y : y + size]
+                )
+                if current_power_lvl > max_power_lvl:
+                    max_power_lvl = current_power_lvl
+                    best_cell = (x + 1, y + 1)
+        return best_cell, max_power_lvl
+
+    def find_general_max(self):
+        best_cell = (0, 0)
+        best_size = 0
+        max_power_lvl = ARBITRARY_LOW
+        for size in range(MAX_SIZE):
+            current_cell, current_power_lvl = self.find_max(size)
+            if current_power_lvl > max_power_lvl:
+                best_cell = current_cell
+                best_size = size
+                max_power_lvl = current_power_lvl
+        return best_cell, best_size
 
 
 def get_hundreds_digit(power_level):
@@ -17,72 +61,18 @@ def get_hundreds_digit(power_level):
     else:
         return int(x[-3])
 
-def find_max(power_levels):
-    best = (0, 0)
-    max_power_level = -10000
-    for x in range(1, GRID_SIZE - 1):
-        for y in range(1, GRID_SIZE - 1):
-            this_power_lvl = -10000
-
-            if x > (GRID_SIZE - 2) or y > (GRID_SIZE - 2):
-                #print('skip', x, y)
-                break
-
-            for i in range(x, x + 3):
-                for j in range(y, y + 3):
-                    this_power_lvl += power_levels[(i, j)]
-            if this_power_lvl > max_power_level:
-                max_power_level = this_power_lvl
-                best = (x, y)
-    return best, max_power_level
-
-def find_max_with_size(power_levels):
-    best = (0, 0)
-    best_size = 0
-    max_power_level = -10000
-    for x in range(1, GRID_SIZE + 1):
-        for y in range(1, GRID_SIZE + 1):
-            for size in range(1, MAX_SIZE + 1):
-
-                this_power_lvl = -10000
-
-                if x > (GRID_SIZE - size) or y > (GRID_SIZE - size):
-                    #print('skip', x, y)
-                    break
-
-                for i in range(x, x + size):
-                    for j in range(y, y + size):
-                        this_power_lvl += power_levels[(i, j)]
-                if this_power_lvl > max_power_level:
-                    max_power_level = this_power_lvl
-                    best = (x, y)
-                    best_size = size
-    return best, best_size
-
 
 def main():
     serial_number = 1308
-    #serial_number = 18
-    #serial_number = 8
-    power_levels = dict()
+    # serial_number = 18
+    # serial_number = 8
 
-    for x in range(1, GRID_SIZE + 1):
-        for y in range(1, GRID_SIZE + 1):
-            rack_id = x + 10
-            power_lvl = rack_id * y
-            power_lvl += serial_number
-            power_lvl *= rack_id
-            power_lvl = get_hundreds_digit(power_lvl)
-            power_lvl -= 5
-            power_levels[(x, y)] = power_lvl
+    grid = Grid(serial_number)
+    best_cell, max_power_lvl = grid.find_max(3)
+    print(f"Part 1: {best_cell}")
 
-    print(max(power_levels.items(), key=operator.itemgetter(1)))
-
-    coord, max_power_level = find_max(power_levels)
-    print('Part 1', coord)
-
-    coord, size = find_max_with_size(power_levels)
-    print('Part 2', coord, size)
+    best_cell, best_size = grid.find_general_max()
+    print(f"Part 2: {best_cell[0]},{best_cell[1]},{best_size}")
 
 
 if __name__ == "__main__":
