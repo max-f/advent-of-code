ccb15aa29bcdb8fd2d7a3bc50b0a1da5c81ff62b
max-f <maaax.fischer@gmail.com>
2019-12-25 10:27:40 +0100

    Some progress for day 20

--- 2019/d20.py
+++ 2019/d20.py
@@ -10,6 +10,9 @@ import bisect
 import networkx as nx
 from pprint import pprint
 
+start = ()
+end = ()
+
 
 def create_board(input_txt):
     board = defaultdict(str)
@@ -18,6 +21,19 @@ def create_board(input_txt):
         for x, c in enumerate(line):
             board[(x, y)] = c
     return board
+
+
+def add_portal(G, portals, portal, pos):
+    global start, end
+
+    if portal == 'AA':
+        start = pos
+    elif portal == 'ZZ':
+        end = pos
+    elif portal in portals:
+        G.add_edge(pos, portals[portal], portal=True)
+    else:
+        portals[portal] = pos
 
 
 def part1() -> int:
@@ -31,12 +47,54 @@ def part2() -> int:
 def main():
     input_txt = get_input(83)
 
+    G = nx.Graph()
+    portals = {}
+
     board = create_board(input_txt)
     width = max(x for x, y in board.keys())
     height = max(y for x, y in board.keys())
 
-    # for x in range(width):
-    # G = nx.Graph()
+    for x in range(width):
+        for y in range(height):
+            c_at_pos = board[(x, y)]
+            if c_at_pos != '.':
+                continue
+
+            right = board[(x + 1, y)]
+            if right == '.':
+                G.add_edge((x, y), (x + 1, y))
+            elif right.isupper():
+                portal = right + board[(x + 2, y)]
+                if portal.isupper():
+                    add_portal(G, portals, portal, (x, y))
+
+            down = board[(x, y + 1)]
+            if down == '.':
+                G.add_edge((x, y), (x, y + 1))
+            elif down.isupper():
+                portal = down + board[(x, y + 2)]
+                if portal.isupper():
+                    add_portal(G, portals, portal, (x, y))
+
+            up = board[(x, y - 1)]
+            if up.isupper():
+                portal = up + board[(x, y - 2)]
+                if portal.isupper():
+                    add_portal(G, portals, portal, (x, y))
+
+            left = board[(x - 1, y)]
+            if left.isupper():
+                portal = left + board[(x - 2, y)]
+                if portal.isupper():
+                    add_portal(G, portals, portal, (x, y))
+
+    print(G.edges.data())
+    print(f'Start pos: {start}')
+    print(f'End pos: {end}')
+    shortest_path = nx.shortest_path(G, start, end)
+    result = nx.shortest_path_length(G, start, end)
+    pprint(shortest_path)
+    print(f'Part 1: {result}')
 
 
 if __name__ == "__main__":
