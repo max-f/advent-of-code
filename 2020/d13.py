97dee975b891ddc0dbacec3a7db35dadfb89750b
max-f <maaax.fischer@gmail.com>
2021-12-01 13:49:39 +0100

    Add stuff to day 13 of 2020

--- 2020/d13.py
+++ 2020/d13.py
@@ -17,16 +17,18 @@ def part2(buses: list[int]) -> int:
     """
     Only by looking at hints from AoC reddit:
     Compare https://brilliant.org/wiki/chinese-remainder-theorem/
+    Or https://rosettacode.org/wiki/Chinese_remainder_theorem
     """
     x = 0
     N = math.prod(bus for bus in buses if bus)
-    for i, bus in enumerate(buses):
-        if not bus:
+    for i, n in enumerate(buses):
+        if not n:
             continue
-        y_i = N // bus
+        a_i = -i % n
+        y_i = N // n
         # See https://docs.python.org/3/library/functions.html#pow
-        z_i = pow(y_i, -1, bus)
-        x += (-i) * y_i * z_i
+        z_i = pow(y_i, -1, n)
+        x += a_i * y_i * z_i
     return x % N
 
 
@@ -35,6 +37,11 @@ def main():
     line_1, line_2 = input_txt.split("\n")
     depart_time = int(line_1)
     buses = [int(b) if b != "x" else None for b in line_2.split(",")]
+
+    bcrap = [(x,((x-i)%x)) for i,x in enumerate(buses) if x]
+    ns, bs = zip(*bcrap)
+    print(buses)
+    print(ns, bs)
 
     print(part1(depart_time, buses))
     print(part2(buses))
