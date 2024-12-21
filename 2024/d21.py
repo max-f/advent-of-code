e4c32d77391d2b2f0070ad9d5cce30f6d1e42004
max-f <maaax.fischer@gmail.com>
2024-12-21 22:58:33 +0100

    Add solution of day 21 p2 and re-use memoization also for p1

--- 2024/d21.py
+++ 2024/d21.py
@@ -2,17 +2,20 @@
 
 from collections import deque
 from itertools import product
+import functools
 
 from utils import utils
 
-num_keypad = [
+DIRECTIONS = [((-1, 0), "^"), ((1, 0), "v"), ((0, -1), "<"), ((0, 1), ">")]
+
+NUM_KEYPAD = [
     ["7", "8", "9"],
     ["4", "5", "6"],
     ["1", "2", "3"],
     [None, "0", "A"],
 ]
 
-robot_keypad = [
+ROBOT_KEYPAD = [
     [None, "^", "A"],
     ["<", "v", ">"],
 ]
@@ -22,13 +25,14 @@ def compute_sequences(keypad):
     pos = {}
     for r in range(len(keypad)):
         for c in range(len(keypad[r])):
-            if keypad[r][c] is not None: pos[keypad[r][c]] = (r, c)
+            if keypad[r][c] is not None:
+                pos[keypad[r][c]] = (r, c)
 
     seqs = {}
     for start in pos:
         for end in pos:
             if start == end:
-                seqs[(start, end)] = ['A']
+                seqs[(start, end)] = ["A"]
                 continue
 
             possibilities = []
@@ -36,11 +40,16 @@ def compute_sequences(keypad):
             shortest = float("inf")
             while q:
                 (r, c), moves = q.popleft()
-                for nr, nc, nm in [(r - 1, c, "^"), (r + 1, c, "v"), (r, c - 1, "<"), (r, c + 1, ">")]:
-                    if nr < 0 or nc < 0 or nr >= len(keypad) or nc >= len(keypad[0]): continue
-                    if keypad[nr][nc] is None: continue
+
+                for (dr, dc), nm in DIRECTIONS:
+                    nr, nc = r + dr, c + dc
+                    if nr < 0 or nc < 0 or nr >= len(keypad) or nc >= len(keypad[0]):
+                        continue
+                    if keypad[nr][nc] is None:
+                        continue
                     if keypad[nr][nc] == end:
-                        if shortest < len(moves) + 1: break
+                        if shortest < len(moves) + 1:
+                            break
                         shortest = len(moves) + 1
                         possibilities.append(moves + nm + "A")
                     else:
@@ -53,49 +62,41 @@ def compute_sequences(keypad):
     return seqs
 
 
-num_seqs = compute_sequences(num_keypad)
-
-robot_seqs = compute_sequences(robot_keypad)
-robot_seq_lengths = {k: len(v[0]) for k, v in robot_seqs.items()}
-
-
 def solve(string, seqs):
     options = [seqs[(x, y)] for x, y in zip("A" + string, string)]
     return ["".join(x) for x in product(*options)]
 
 
-def find_shortest_string(nested_list):
-    def flatten(lst):
-        result = []
-        for item in lst:
-            if isinstance(item, list):
-                result.extend(flatten(item))
-            elif isinstance(item, str):
-                result.append(item)
-        return result
-
-    flat_strings = flatten(nested_list)
-
-    if not flat_strings:
-        return None
-
-    return min(flat_strings, key=len)
-
 def part1(codes):
     total = 0
     for code in codes:
         numeric_part = int(code[:-1])
+        inputs = solve(code, num_seqs)
+        length = min(calc_length(code, depth=2) for code in inputs)
+        total += length * numeric_part
+    return total
 
-        first_enc = solve(code, num_seqs)
-        second_enc = [solve(enc, robot_seqs) for enc in first_enc]
-        third_enc = [[solve(enc, robot_seqs) for enc in xs] for xs in second_enc]
 
-        # print(third_enc)
-        minimal_encoding = find_shortest_string(third_enc)
-        print(len(minimal_encoding))
+@functools.cache
+def calc_length(code, depth=25):
+    if depth == 1:
+        return sum(
+            robot_seq_lengths[(start, end)] for start, end in zip("A" + code, code)
+        )
+    length = 0
+    for start, end in zip("A" + code, code):
+        length += min(
+            calc_length(subseq, depth - 1) for subseq in robot_seqs[(start, end)]
+        )
+    return length
 
-        complexity = len(minimal_encoding) * numeric_part
-        total += complexity
+
+def part2(codes):
+    total = 0
+    for code in codes:
+        inputs = solve(code, num_seqs)
+        length = min(map(calc_length, inputs))
+        total += length * int(code[:-1])
     return total
 
 
@@ -103,8 +104,13 @@ def main() -> None:
     input_txt = utils.get_input(21)
     codes = input_txt.strip().split("\n")
 
+    global num_seqs, robot_seqs, robot_seq_lengths
+    num_seqs = compute_sequences(NUM_KEYPAD)
+    robot_seqs = compute_sequences(ROBOT_KEYPAD)
+    robot_seq_lengths = {k: len(v[0]) for k, v in robot_seqs.items()}
+
     print(f"Part1: {part1(codes)}")
-    # print(f"Part2: {part2(grid, G, start, end)}")
+    print(f"Part2: {part2(codes)}")
 
 
 if __name__ == "__main__":
