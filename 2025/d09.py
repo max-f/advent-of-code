6a308546d6e5095089e151e7e1e262dffc8d0d8e
max-f <maaax.fischer@gmail.com>
2025-12-10 21:37:40 +0100

    Add solution of day 9 p2

--- 2025/d09.py
+++ 2025/d09.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-
 import itertools
 
 from utils import utils
+from shapely import Polygon
 
 """
 Code for https://adventofcode.com/2025/day/9
@@ -11,11 +11,23 @@ Code for https://adventofcode.com/2025/day/9
 
 
 def part2(points):
-    min_x = min(points, key=lambda p: p[0])
-    min_y = min(points, key=lambda p: p[1])
-    max_x = max(points, key=lambda p: p[0])
-    max_y = max(points, key=lambda p: p[1])
-    print(min_x, min_y, max_x, max_y)
+    map = Polygon(points)
+    max_points = -1
+    all_pairs = itertools.combinations(points, 2)
+    for p1, p2 in all_pairs:
+        x1, y1 = p1
+        x2, y2 = p2
+        min_x = min(x1, x2)
+        min_y = min(y1, y2)
+        max_x = max(x1, x2)
+        max_y = max(y1, y2)
+        curr_poly = Polygon(
+            ((min_x, min_y), (max_x, min_y), (max_x, max_y), (min_x, max_y))
+        )
+        if map.contains(curr_poly):
+            area = (abs(x2 - x1) + 1) * (abs(y2 - y1) + 1)
+            max_points = max(max_points, area)
+    return max_points
 
 
 def main():
